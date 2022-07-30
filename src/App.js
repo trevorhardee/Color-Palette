335624504273a3db92506f838027db1826734bfc
trevorhardee <109926865+trevorhardee@users.noreply.github.com>
2022-07-30 09:28:18 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ColorPalette from './ColorPalette/ColorPalette';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPalette></ColorPalette>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ColorPalette/ColorPalette.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+export default class ColorPalette extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount(){
+        //const grid = drawGrid();
+        //this.setState({grid: grid});
+    }
+
+    render() {
+        //const {grid, mouseIsPressed} = this.state;
+
+        return (
+            <></>
+        )
+    }
+}
